f268c7ffdf3c419aa6313bfe39232421a54860cd
Tristan Van Maren <tm.vanmaren@gmail.com>
2017-01-17 15:36:01 -0700

    :art: remove old code

--- caesarShift.js
+++ caesarShift.js
@@ -4,45 +4,6 @@ const caesar = {};
 caesar.encode = encode;
 caesar.decode = decode;
 
-// function caesarRing(unicode, min, max, shift, add) {
-//   let ring = max - min;
-//   let newCode = unicode - min;
-//   if (newCode < shift && !add) {
-//     newCode += max;
-//   }
-//   unicode = (unicode + shift) % ring;
-//   unicode += min;
-//   return unicode;
-// }
-
-// function caesarShift(message, modifier, addBool) {
-//
-//   let charArray = message.split('');
-//   charArray = charArray.map((char) => {
-//     let code = char.charCodeAt(0);
-//     let upperCaseRange = {
-//       floor: 65,
-//       ceil: 90
-//     };
-//     let lowerCaseRange = {
-//       floor: 97,
-//       ceil: 122
-//     };
-//     if (code >= upperCaseRange.floor &&
-//       code <= upperCaseRange.ceil) {
-//       code = caesarRing(code, upperCaseRange.floor, upperCaseRange.ceil, modifier, addBool);
-//
-//     } else if (code >= lowerCaseRange.floor &&
-//       code <= lowerCaseRange.ceil) {
-//       code = caesarRing(code, lowerCaseRange.floor, lowerCaseRange.ceil, modifier, addBool);
-//     }
-//     return String.fromCharCode(code);
-//   });
-//
-//   return charArray.join('');
-//
-// }
-
 function codeRing(char, shift, min) { //lower:97-122,upper:65-90
   let code = char.charCodeAt(0) + shift;
   if (code >= (min + 26)) {
