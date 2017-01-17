eaaa8dd8897bc022d3acf2f61401cd94724e1c2b
Tristan Van Maren <tm.vanmaren@gmail.com>
2017-01-17 13:42:17 -0700

    :sparkles: encoder functional

--- caesarShift.js
+++ caesarShift.js
@@ -1,4 +1,36 @@
+'use strict';
+const caesar = {};
 
+caesar.encode = encode;
+caesar.decode = decode;
 
+function encode(message) {
+  let charArray = message.split('');
+  charArray = charArray.map((char) => {
+    let code = char.charCodeAt();
+    let upperCaseRange = {
+      lowest: 65,
+      highest: 90
+    };
+    let lowerCaseRange = {
+      lowest: 97,
+      highest: 122
+    };
+    if (code >= upperCaseRange.lowest &&
+      code <= upperCaseRange.highest) {
+      code = (code + 3) % upperCaseRange.highest;
 
-module.exports = {}
+    } else if (code >= lowerCaseRange.lowest &&
+      code <= lowerCaseRange.highest) {
+      code = (code + 3) % lowerCaseRange.highest;
+    }
+    return String.fromCharCode(code);
+  });
+  return charArray.join('');
+}
+
+function decode(message) {
+  return message;
+}
+
+module.exports = caesar;
