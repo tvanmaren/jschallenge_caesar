406586dd3a54358278ae3bceae9b44d0aabc6c2a
Tristan Van Maren <tm.vanmaren@gmail.com>
2017-01-17 13:45:35 -0700

    :sparkles: convert encoder to caesarShift -- reuse code for decoding

--- caesarShift.js
+++ caesarShift.js
@@ -4,7 +4,8 @@ const caesar = {};
 caesar.encode = encode;
 caesar.decode = decode;
 
-function encode(message) {
+function caesarShift(message, modifier) {
+
   let charArray = message.split('');
   charArray = charArray.map((char) => {
     let code = char.charCodeAt();
@@ -18,19 +19,25 @@ function encode(message) {
     };
     if (code >= upperCaseRange.lowest &&
       code <= upperCaseRange.highest) {
-      code = (code + 3) % upperCaseRange.highest;
+      code = (code + modifier) % upperCaseRange.highest;
 
     } else if (code >= lowerCaseRange.lowest &&
       code <= lowerCaseRange.highest) {
-      code = (code + 3) % lowerCaseRange.highest;
+      code = (code + modifier) % lowerCaseRange.highest;
     }
     return String.fromCharCode(code);
   });
+
   return charArray.join('');
+
+}
+
+function encode(message) {
+  return caesarShift(message, 3);
 }
 
 function decode(message) {
-  return message;
+  return caesarShift(message, -3);
 }
 
 module.exports = caesar;
