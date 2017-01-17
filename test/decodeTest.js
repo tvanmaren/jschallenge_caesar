c7a3798c6ea190e1d61f777f6411d0bdd6e91255
Tristan Van Maren <tm.vanmaren@gmail.com>
2017-01-17 13:48:22 -0700

    :construction: add edge cases to tests

--- test/decodeTest.js
+++ test/decodeTest.js
@@ -1,12 +1,18 @@
 const caesar = require('../caesarShift.js');
 const expect = require('chai').expect;
 
-describe('Decoder',function(){
-    it('Function decode Should exist', function() {
-        expect(typeof caesar.decode).to.equal('function');
-    });
-    it('Should Decode Correctly', function(){
-        expect(caesar.decode('Yhql, ylgl, ylfl.')).to.equal('Veni, vidi, vici.');
-        expect(caesar.decode('Dohd ldfwd hvw.')).to.equal('Alea iacta est.');
-    });
+describe('Decoder', function () {
+  it('Function decode Should exist', function () {
+    expect(typeof caesar.decode).to.equal('function');
+  });
+  it('Should Decode Correctly', function () {
+    expect(caesar.decode('Yhql, ylgl, ylfl.')).to.equal('Veni, vidi, vici.');
+    expect(caesar.decode('Dohd ldfwd hvw.')).to.equal('Alea iacta est.');
+  });
+  // Add these edge cases to your test files -
+
+  it('Should Decode A Correctly', function () {
+    expect(caesar.decode('A')).to.equal('W');
+    expect(caesar.decode('a')).to.equal('w');
+  });
 });
--- test/encodeTest.js
+++ test/encodeTest.js
@@ -1,12 +1,16 @@
 const caesar = require('../caesarShift.js');
 const expect = require('chai').expect;
 
-describe('Encoder',function(){
-    it('Function encode should exist', function() {
-        expect(typeof caesar.encode).to.equal('function');
-    });
-    it('Should Encode Correctly', function(){
-        expect(caesar.encode('The fault, dear Brutus, is not in our stars, but in ourselves.')).to.equal('Wkh idxow, ghdu Euxwxv, lv qrw lq rxu vwduv, exw lq rxuvhoyhv.');
-        expect(caesar.encode('Et tu, Brute?')).to.equal('Hw wx, Euxwh?');
-    });
+describe('Encoder', function () {
+  it('Function encode should exist', function () {
+    expect(typeof caesar.encode).to.equal('function');
+  });
+  it('Should Encode Correctly', function () {
+    expect(caesar.encode('The fault, dear Brutus, is not in our stars, but in ourselves.')).to.equal('Wkh idxow, ghdu Euxwxv, lv qrw lq rxu vwduv, exw lq rxuvhoyhv.');
+    expect(caesar.encode('Et tu, Brute?')).to.equal('Hw wx, Euxwh?');
+  });
+  it('Should Encode Z Correctly', function () {
+    expect(caesar.encode('Z')).to.equal('C');
+    expect(caesar.encode('z')).to.equal('c');
+  });
 });
